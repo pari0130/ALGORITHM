a979b661eae198de92831dd4f2ad6512b9777c85
조동휘 <pari0130@jinotech.com>
2020-11-26 10:20:28 +0900

    study 2020-11-16

---
+++ PYTHON/A.인프런/04. 자료구조 활용(스택,큐,해쉬,힙)/4) 후위 연산(스택)/AA.py
@@ -0,0 +1,19 @@
+
+# 04. 자료구조 활용(스택,큐,해쉬,힙)
+
+'''
+352+*9-
+
+3*(5+2)-9
+3+5*2-9
+
+
+'''
+import sys
+path = "D:/03.PERSNAL/00.STUDY/ALGORITHM/PYTHON/A.인프런/04. 자료구조 활용(스택,큐,해쉬,힙)/4) 후위 연산(스택)"
+file = "/input.txt"
+sys.stdin = open(path + file, "r")
+
+a = input()
+stack = []
+res = ''
