a26ede7cfeb88eebddc66d906b17b448c2bbe311
조동휘 <pari0130@jinotech.com>
2021-03-08 19:04:13 +0900

    프로그래머스 프린트

---
+++ PYTHON/A.인프런/08.정렬모음/1)버블정렬/AA.py
@@ -0,0 +1,35 @@
+
+'''
+
+'''
+
+
+def bubble_sort1(arr):  # 오름차순
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] < arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+def bubble_sort2(arr):  # 오름차순
+    for i in range(len(arr)):
+        for j in range(i):
+            if arr[j] < arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+def bubble_sort3(arr):  # 내림차순
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+print(bubble_sort1([2, 1, 3, 2]))
+print(bubble_sort2([2, 1, 3, 2]))
---
+++ PYTHON/A.인프런/08.정렬모음/2)퀵정렬/AA.py
@@ -0,0 +1,8 @@
+
+'''
+
+'''
+import sys
+path = "D:/03.PERSNAL/00.STUDY/ALGORITHM/PYTHON/A.인프런/07.동적계획법/1) 동적계획법 네트워크선 자르기"
+file = "/input.txt"
+sys.stdin = open(path + file, "r")
