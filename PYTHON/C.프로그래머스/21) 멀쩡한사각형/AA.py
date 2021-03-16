d67fd4deb19ea5703b2f72115a40781988849dd5
pari0130@gmail.com <pari0130@gmail.com>
2021-03-16 22:28:59 +0900

    프로그래머스 멀쩡한사각형

--- PYTHON/C.프로그래머스/21) 멀쩡한사각형/AA.py
+++ PYTHON/C.프로그래머스/21) 멀쩡한사각형/AA.py
@@ -1,45 +1,14 @@
 
 '''
+2*3 배열일때 지나가는 점선의 개수는
+2+3 - 최대공약수 만큼의 크기가 계산된다.
 
 '''
-from collections import deque
-
-
-# 현재 위치의 상하좌우 좌표 값 위치 구하기 위한 변수
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+import math
 
 
 def solution(w, h):
-    answer = 1
-    n = w*h
-    board = [[0] * w for _ in range(h)]
-    dis = [[0] * w for _ in range(h)]  # dis 배열 8*16 길이 배열
-    Q = deque()
-    Q.append((0, 0))
-    board[0][0] = 1  # 이동된 구간은 1로 초기화
-
-    print(dis, end='\\n')
-
-    while Q:
-        tmp = Q.popleft()
-        for i in range(4):  # 4방향 탐색
-            # x = 기존 x 보다 커야함
-            # y = 기존 y 보다 커야함
-            x = tmp[0] + dx[i]
-            y = tmp[1] + dy[i]
-            # x, y 의 위치는 dx, dy 값으로 인해 현재 위치 상하좌우 값임
-            if 0 <= x <= 6 and 0 <= y <= 6 and board[x][y] == 0:
-                board[x][y] = 1
-                dis[x][y] = dis[tmp[0]][tmp[1]] + 1
-                Q.append((x, y))
-
-    if dis[6][6] == 0:  #
-        print(-1)
-    else:
-        print(dis[6][6])
-
-    return answer
+    return w*h - (w+h-math.gcd(w, h))
 
 
 print("답 : ", solution(8, 12))  # 80
