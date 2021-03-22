cbc5e712bfb909abb20115d58a009e1aaed3a954
pari0130@gmail.com <pari0130@gmail.com>
2021-03-22 21:32:49 +0900

    프로그래머스 2021-03-22

--- PYTHON/C.프로그래머스/29) 더맵게(heap)/AA.py
+++ PYTHON/C.프로그래머스/29) 더맵게(heap)/AA.py
@@ -1,43 +1,39 @@
 
 '''
 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+
+나중을 위해 코멘트
+
+힙큐는 heappush 할 경우 알아서 가장 작은 수 순서대로 정렬된 결과를 return 
+
+전달받은 list 를 힙에 넣어서 힘큐로 변환 : hq.heapify(리스트))
+가장 앞의 작은 수를 return : hq.heappop(리스트)
+힙에 데이터를 넣는 방법 : hq.heappush(리스트, 2)
+
 '''
 
 import heapq as hq  # 노드 힙 구조를 사용하기 위해 heapq import
 
 
-def scoville_list(tmp, scoville):
-    for i in scoville:
-        hq.heappush(tmp, i)
-
-    return scoville
-
-
 def solution(scoville, K):
     answer = 0
-    tmp = []
-    scoville_num = 0
-    tmp = scoville_list(tmp, scoville)
+    n = 0
+    hq.heapify(scoville)
 
-    while min(tmp) > 7:
-        scoville_num = tmp.pop(0) + (tmp.pop(0) * 2)
-        tmp = scoville_list(tmp, scoville)
+    while scoville:
+        first = hq.heappop(scoville)
 
-    print(tmp)
+        if first >= K:
+            break
 
-    # while True:
-    #     hq.heapify(scoville)
+        if len(scoville) <= 0:
+            return -1
 
-    #     n = int(input())
-    #     if n == -1:  # -1일 경우 종료
-    #         break
-    #     if n == 0:  # 0일 경우 입력되었던 숫자 중 최소힙을 출력
-    #         if len(a) == 0:
-    #             print(-1)
-    #         else:
-    #             print(hq.heappop(a))  # root node 출력 (최소힙을 출력하는 방법)
-    #     else:
-    #         hq.heappush(a, n)
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + second * 2)
+        answer += 1
+
+    print(scoville)
 
     return answer
 
