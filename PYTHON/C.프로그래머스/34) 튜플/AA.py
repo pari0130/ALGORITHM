4be17b8e6a47dcda13e11ef6a99ea055c694451e
pari0130@gmail.com <pari0130@gmail.com>
2021-03-23 22:40:52 +0900

    프로그래머스 2021-03-23

---
+++ PYTHON/C.프로그래머스/34) 튜플/AA.py
@@ -0,0 +1,21 @@
+
+'''
+https://programmers.co.kr/learn/courses/30/lessons/42577
+
+'''
+
+
+'''
+포인트는 가벼운사람과 무거운사람이 한 배에 타도록 계산을 해야한다.
+
+'''
+
+
+def solution(brown, yellow):
+    answer = []
+    return answer
+
+
+print("답 : ", solution(10, 2))  # [4,3]
+print("답 : ", solution(8, 1))  # [3,3]
+print("답 : ", solution(24, 24))  # [8,6]
