e60547f6baa3bc620ef8db683a18247f7bd23389
조동휘 <pari0130@gmail.com>
2021-06-17 18:21:39 +0900

    코틀린 알고리즘 - 단어뒤집기 ing..

---
+++ KOTLIN/src/STEP01/04_단어뒤집기.kt
@@ -0,0 +1,24 @@
+package STEP01
+
+import java.util.Scanner
+
+// https://cote.inflearn.com/contest/10/problem/01-01
+
+fun main() {
+    val kb: Scanner = Scanner(System.`in`)
+    val n : Int = kb.nextInt()
+    val str : ArrayList<String> = ArrayList()
+
+    for(i in 0..n){
+        str[i] = kb.next()
+    }
+
+    // println(solution(str, c))
+    // val name = sc.nextLine()
+    // val age = sc.nextInt()
+}
+
+private fun solution() : ArrayList {
+//    var answer = 0
+//    return answer
+}
