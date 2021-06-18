a3f7f76c6f82e73e2ff9838862ae0742ed92c181
조동휘 <pari0130@gmail.com>
2021-06-18 09:28:08 +0900

    코틀린 알고리즘 - 중복 문자 제거

---
+++ KOTLIN/src/STEP01/06_중복문자제거.kt
@@ -0,0 +1,21 @@
+package STEP01
+
+import java.lang.StringBuilder
+import java.util.Scanner
+
+// 중복된 문자열 제거 출력
+fun main() {
+    val kb: Scanner = Scanner(System.`in`)
+    val str = kb.next()
+    println(solution(str))
+}
+
+private fun solution(str: String): String {
+    var answer = ""
+    //
+    for(i in 1..str.length){
+        if(str.indexOf(str.get(i)) == i) answer += str.get(i)
+    }
+
+    return answer
+}
