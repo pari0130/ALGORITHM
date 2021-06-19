02e3d5ea98b3fa1efd67e8a9301b5dc24d7ed8b6
조동휘 <pari0130@jinotech.com>
2021-06-19 19:16:57 +0900

    코틀린 알고리즘 - 문자열압축

---
+++ KOTLIN/src/STEP01/11_문자열압축.kt
@@ -0,0 +1,32 @@
+package STEP01
+
+import java.lang.StringBuilder
+import java.util.Scanner
+
+// 문자열 압축,
+// KKHSSSSSSSE : K2HS7E
+fun main() {
+    val kb: Scanner = Scanner(System.`in`)
+    val str = kb.next()
+    println(solution(str))
+
+}
+
+private fun solution(str: String): String {
+    val answer = StringBuilder()
+    val s = "$str "
+    var cnt = 1
+
+    for (i in str.indices) {
+        if (s.get(i) == s.get(i + 1)) {
+            cnt++
+        } else {
+            answer.append(s.get(i))
+            if (cnt > 1) {
+                answer.append(cnt.toString())
+            }
+            cnt = 1
+        }
+    }
+    return answer.toString()
+}
