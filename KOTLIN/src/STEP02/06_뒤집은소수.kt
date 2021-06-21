549a4559fd3eca48e9e21752d88cf4561712b75d
조동휘 <pari0130@gmail.com>
2021-06-21 09:14:02 +0900

    코틀린 알고리즘 - 뒤집은 소수 ing...

---
+++ KOTLIN/src/STEP02/06_뒤집은소수.kt
@@ -0,0 +1,26 @@
+package STEP02
+
+import java.util.*
+
+// https://cote.inflearn.com/contest/10/problem/02-06
+/*
+    in
+    9
+    32 55 62 20 250 370 200 30 100
+
+    out
+    23 2 73 2 3
+*/
+private fun solution(n: Int): Int {
+    var answer = 0
+    val ch = IntArray(n + 1)
+
+    return answer
+}
+
+fun main() {
+    val kb = Scanner(System.`in`)
+    val n = kb.nextInt()
+
+    println(solution(n))
+}
