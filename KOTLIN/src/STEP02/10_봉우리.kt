770f8c40dc9d5a30db6b4f733f0ecc14f7cc0b59
donghwi <pari0130@gmail.com>
2021-06-29 08:43:00 +0900

    코틀린 알고리즘 - 봉우리

---
+++ KOTLIN/src/STEP02/10_봉우리.kt
@@ -0,0 +1,53 @@
+package STEP02
+
+import java.util.*
+
+// https://cote.inflearn.com/contest/10/problem/02-10
+// 현재 위치가 상하좌우 값 보다 큰 위치(봉우리) 가 몇개 있는지 확인
+/*
+in
+5
+5 3 7 2 3
+3 7 1 6 1
+7 2 5 3 4
+4 3 6 4 1
+8 7 3 5 2
+
+out
+10
+*/
+private fun solution(n: Int, arr: Array<IntArray>): Int {
+    var answer = 0
+    val dx = listOf(-1, 0, 1, 0)
+    val dy = listOf(0, 1, 0, -1)
+
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            var flag = true
+            // 4방향 탐색 부분
+            for (k in 0 until 4) {
+                val nx = i + dx[k]
+                val ny = j + dy[k]
+                // nx >= 0 && nx < n => nx in 0 until n
+                if (nx in 0 until n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+                    flag = false
+                    break
+                }
+            }
+            if (flag) answer++
+        }
+    }
+    return answer
+}
+
+fun main() {
+    val kb = Scanner(System.`in`)
+    val n = kb.nextInt()
+    val arr = Array(n) { IntArray(n) }
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            arr[i][j] = kb.nextInt()
+        }
+    }
+    print(solution(n, arr))
+}
