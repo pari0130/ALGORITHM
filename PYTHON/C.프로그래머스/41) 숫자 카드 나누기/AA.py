af673bc88a33b65eb71fd97319c1438fd35fbb63
coheim <pari0130@gmail.com>
2023-03-05 21:06:33 +0900

    [프로그래머스][py] 숫자 카드 나누기

--- PYTHON/C.프로그래머스/41) 숫자 카드 나누기/AA.py
+++ PYTHON/C.프로그래머스/41) 숫자 카드 나누기/AA.py
@@ -1,12 +1,39 @@
 '''
 https://school.programmers.co.kr/learn/courses/30/lessons/135807
-
-
 '''
-
+import numpy as np
 
 def solution(arrayA, arrayB):
     answer = 0
+    gcdA = int(np.gcd.reduce(arrayA))
+    gcdB = int(np.gcd.reduce(arrayB))
+    answerA = 0
+    answerB = 0
+
+    if gcdA > 1:
+        for b in arrayB:
+            if b > gcdA and b % gcdA == 0:
+                answerA = 0
+                break
+            else:
+                answerA = gcdA
+
+    if gcdB > 1:
+        print("22222")
+        for a in arrayA:
+            if a > gcdB and a % gcdB == 0:
+                answerB = 0
+                break
+            else:
+                answerB = gcdB
+
+    if answerA > answerB:
+        answer = answerA
+    elif answerB > answerA:
+        answer = answerB
+    else:
+        answer = 0
+
     return answer
 
 
